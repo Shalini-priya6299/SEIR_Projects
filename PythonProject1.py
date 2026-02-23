5d47c89999f5cceb93c117327d6442f7961c9de7
Shalini Priya <shalinipriya9155@gmail.com>
2026-02-23 21:27:02 +0530

    Add files via upload

---
+++ PythonProject1.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin 
+
+url = input("Enter URL: ")
+
+response = requests.get(url)
+data = BeautifulSoup(response.text, "html.parser")
+
+print("PAGE TITLE:")
+if data.title:
+    print(data.title.get_text())
+
+print("\nPAGE BODY:")
+if data.body:
+  print(data.body.get_text())
+
+print("\nLINKS:")
+for link in data.find_all('a'):
+   if link.get("href"):
+      print(urljoin(url, link.get('href')))
