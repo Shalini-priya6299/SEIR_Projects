870286675a97cab89fe6c6f18b3ff47d3ad326b3
Shalini Priya <shalinipriya9155@gmail.com>
2026-02-23 21:34:04 +0530

    Add error handling for missing title, body, and links

--- PythonProject1.py
+++ PythonProject1.py
@@ -10,12 +10,18 @@ data = BeautifulSoup(response.text, "html.parser")
 print("PAGE TITLE:")
 if data.title:
     print(data.title.get_text())
+else:
+    print("No Title Found!")
 
 print("\nPAGE BODY:")
 if data.body:
   print(data.body.get_text())
+else:
+   print("No Body Found!")
 
 print("\nLINKS:")
 for link in data.find_all('a'):
-   if link.get("href"):
+    if link.get("href"):
       print(urljoin(url, link.get('href')))
+    else:
+      print("No link Found!")
